4a657c30d08336c6621503712cba0e7f8873a9f2
keke0123 <keke0123@naver.com>
2019-10-28 18:38:49 +0900

    mobx-router 테스트중

--- src/App.js
+++ src/App.js
@@ -2,21 +2,27 @@ import React from 'react';
 import './App.css';
 import TopLayout from './layouts';
 // mobx
-import {Provider} from 'mobx-react';
+import { Provider } from 'mobx-react';
 import RootStore from './stores';
 const rootStore = new RootStore();
+// mobx-router
+import { MobxRouter, RouterStore, startRouter } from 'mobx-router';
+rootStore = {
+    ...rootStore,
+    router: new RouterStore()
+};
 // import CounterStore from './stores/counter';
 // const counter = new CounterStore();
 
 function App() {
-  return (
-    <Provider rootStore={rootStore}>
-      <div className="App">
-        App page
-        <TopLayout></TopLayout>
-      </div>
-    </Provider>
-  );
+    return (
+        <Provider rootStore={rootStore}>
+            <div className="App">
+                App page
+                <TopLayout></TopLayout>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;
